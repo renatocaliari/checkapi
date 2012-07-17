0e90064d2f81bae983e1aecb7c08106b48461205
renatoac <renatoac82@gmail.com>
2012-07-17 11:45:04 -0300

    Adding examples

---
+++ test/api/apiExample.vows.js
@@ -0,0 +1,28 @@
+var scenarioVows = require('../../lib/helper/scenarioVows'),
+    vows = require('vows'),
+    assert = require('assert'),
+    config = require('../../test/api/configApiExample'),
+
+vows.describe('API').addBatch(
+    {
+        'A call to resource': {
+            '/places/{placeId}/reviews/new': {
+                '[positive]': scenarioVows.testCase('/places/{placeId}/reviews/new', 'positive',
+                    function(error, data, response, scenario) {
+                        var dataJson = JSON.parse(data),
+                        message = dataJson.message;
+
+                        assert.include(data, 'REVIEW OK');
+                    }
+                ),
+                 '[negative]': scenarioVows.testCase('/places/{placeId}/reviews/new', 'negative',
+                    function(error, data, response, scenario) {
+                        var dataJson = JSON.parse(data),
+                        message = dataJson.message;
+
+                        assert.include(data, 'REVIEW FAIL');
+                    }
+                )
+            }  
+        }
+}).export(module);
