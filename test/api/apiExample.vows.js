e5c341c5bb783960b0a57ecf140298e7b6b519b3
Renato <renatoac82@gmail.com>
2012-08-13 17:24:41 -0300

    Update test/api/apiExample.vows.js

--- test/api/apiExample.vows.js
+++ test/api/apiExample.vows.js
@@ -3,26 +3,9 @@ var scenarioVows = require('../../lib/helper/scenarioVows'),
     assert = require('assert'),
     config = require('../../test/api/configApiExample');
 
-vows.describe('API').addBatch(
-    {
+vows.describe('API').addBatch({
         'A call to resource': {
-            '/places/{placeId}/reviews/new': {
-                '[positive]': scenarioVows.testCase('/places/{placeId}/reviews/new', 'positive',
-                    function(error, data, response, scenario) {
-                        var dataJson = JSON.parse(data),
-                        message = dataJson.message;
-
-                        assert.include(data, 'REVIEW OK');
-                    }
-                ),
-                 '[negative]': scenarioVows.testCase('/places/{placeId}/reviews/new', 'negative',
-                    function(error, data, response, scenario) {
-                        var dataJson = JSON.parse(data),
-                        message = dataJson.message;
-
-                        assert.include(data, 'REVIEW FAIL');
-                    }
-                )
-            }  
+            '/places/{placeId}/photos/new': scenarioVows.testCase('/places/{placeId}/photos/new'),
+            '/search/places/byaddress': scenarioVows.testCase('/search/places/byaddress')
         }
 }).export(module);
